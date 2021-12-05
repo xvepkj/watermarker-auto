42b133e1eac59ac451f5f90f855645f74851bddb
xvepkj <pkjrockzz@gmail.com>
2021-12-06 00:35:50 +0530

    corrected config, changed print statement to make sense

--- Watermark.py
+++ Watermark.py
@@ -62,7 +62,7 @@ def copyright_apply(input_image_path,
  c_text.putalpha(100)
  photo.paste(c_text, pos, c_text)
  photo.save(output_image_path)
- print("DONE")
+ print(input_image_path)
  
  
 folderPath = StringVar()
